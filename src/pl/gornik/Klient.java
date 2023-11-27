5b88e85648bb7758f0c897bba9454b0bf534decb
Bartosz Krawczyk <144348029+Krawczyk0@users.noreply.github.com>
2023-11-27 18:50:57 +0100

    Update Klient.java

--- src/pl/gornik/Klient.java
+++ src/pl/gornik/Klient.java
@@ -1,18 +1,19 @@
 package pl.gornik;
-
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
-public class Klient {
-    private String imie;
-    private String nazwisko;
+
+class Klient implements Serializable {
+    public String imie;
+    public String nazwisko;
     private List<Polisa> polisy;
 
-
     public Klient(String imie, String nazwisko) {
-        this.imie=imie;
-        this.nazwisko=nazwisko;
-        this.polisy=new ArrayList<>();
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.polisy = new ArrayList<>();
     }
 
     public Klient(String imie, String nazwisko, List<Polisa> polisy) {
@@ -20,36 +21,76 @@ public class Klient {
         this.nazwisko = nazwisko;
         this.polisy = polisy;
     }
+
     public void dodajPolise(String typ, double skladka) {
-        Polisa nowaPolisa=new Polisa(typ, skladka);
+        Polisa nowaPolisa = new Polisa(typ, skladka);
         polisy.add(nowaPolisa);
-        System.out.println("Dodano polise dla klienta: " + imie + " " + nazwisko);
+        System.out.println("Dodano polisę dla klienta: " + imie + " " + nazwisko);
     }
+
     public void usunPolise(String typ) {
         polisy.removeIf(polisa -> polisa.getTyp().equals(typ));
-        System.out.println("Usunieto polise dla klienta: " + imie + " " + nazwisko);
+        System.out.println("Usunięto polisę typu " + typ + " dla klienta: " + imie + " " + nazwisko);
     }
+
     public void wyswietlPolisy() {
         System.out.println("Polisy klienta " + imie + " " + nazwisko + ":");
         for (Polisa polisa : polisy) {
-            System.out.println("Typ: " + polisa.getTyp() + ",Skladka: " + polisa.getSkladka());
+            System.out.println("Typ: " + polisa.getTyp() + ", Składka: " + polisa.getSkladka());
         }
     }
 
-    public double obliczSkladke() {
-        double sumaSkladek=0;
-        for(Polisa polisa: polisy) {
-            sumaSkladek +=polisa.getSkladka();
+    public double obliczSkladkeSumaryczna() {
+        double sumaSkladek = 0;
+        for (Polisa polisa : polisy) {
+            sumaSkladek += polisa.getSkladka();
         }
         return sumaSkladek;
     }
+
     public static void powitanie() {
-        System.out.println("Witaj w naszej firmie ubezpieczeniowej");
+        System.out.println("Witaj w systemie ubezpieczeniowym!");
     }
+
     public static void zakonczenie() {
-        System.out.println("Dziękujemy za skorzystanie z naszych usług");
+        System.out.println("Dziękujemy za skorzystanie z naszych usług. Życzymy bezpiecznych dni!");
+    }
 
+    public static Klient wczytajKlienta() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj imię klienta: ");
+        String imie = scanner.nextLine();
+        System.out.print("Podaj nazwisko klienta: ");
+        String nazwisko = scanner.nextLine();
+        return new Klient(imie, nazwisko);
+    }
 
-        
+    public static Polisa wczytajNowaPolise() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj typ polisy: ");
+        String typ = scanner.nextLine();
+        System.out.print("Podaj składkę polisy: ");
+        double skladka = scanner.nextDouble();
+        return new Polisa(typ, skladka);
+    }
+
+    public void zapiszDoPliku(String nazwaPliku) {
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(nazwaPliku))) {
+            outputStream.writeObject(this);
+            System.out.println("Dane klienta zapisane do pliku: " + nazwaPliku);
+        } catch (IOException e) {
+            System.err.println("Błąd podczas zapisu do pliku: " + e.getMessage());
+        }
+    }
+
+    public static Klient wczytajZPliku(String nazwaPliku) {
+        try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(nazwaPliku))) {
+            Klient klient = (Klient) inputStream.readObject();
+            System.out.println("Dane klienta wczytane z pliku: " + nazwaPliku);
+            return klient;
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println("Błąd podczas odczytu z pliku: " + e.getMessage());
+            return null;
+        }
     }
 }
