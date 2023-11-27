f7cacaf99e708f69dfead28d71bca7b7b6a294fc
Bartosz Krawczyk <144348029+Krawczyk0@users.noreply.github.com>
2023-11-27 18:50:18 +0100

    Update KlientVIP.java

--- src/pl/gornik/KlientVIP.java
+++ src/pl/gornik/KlientVIP.java
@@ -1,20 +1,31 @@
 package pl.gornik;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class KlientVIP extends Klient {
-    private double znizka;
 
-    public KlientVIP(String imie, String nazwisko, double znizka ){
+
+class KlientVIP extends Klient {
+    private double rabat;
+
+    public KlientVIP(String imie, String nazwisko, double rabat) {
         super(imie, nazwisko);
-        this.znizka=znizka;
+        this.rabat = rabat;
     }
-    public double getZnizka() {
-        return znizka;
+
+    public KlientVIP(String imie, String nazwisko, List<Polisa> polisy, double rabat) {
+        super(imie, nazwisko, polisy);
+        this.rabat = rabat;
+    }
+
+    public double getRabat() {
+        return rabat;
     }
 
     @Override
-    public double obliczSkladke() {
-        double sumaSkladek=super.obliczSkladke();
-        return sumaSkladek -(sumaSkladek * znizka);
-      }
+    public double obliczSkladkeSumaryczna() {
+        double sumaSkladek = super.obliczSkladkeSumaryczna();
+        return sumaSkladek - (sumaSkladek * rabat);
     }
-
+}
