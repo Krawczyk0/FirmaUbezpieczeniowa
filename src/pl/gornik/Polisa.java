d38e364ced87dfabaee86d8c61178d303c073197
Bartosz Krawczyk <144348029+Krawczyk0@users.noreply.github.com>
2023-11-27 18:49:58 +0100

    Update Polisa.java

--- src/pl/gornik/Polisa.java
+++ src/pl/gornik/Polisa.java
@@ -1,17 +1,24 @@
 package pl.gornik;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class Polisa {
+
+
+class Polisa implements Serializable {
     private String typ;
     private double skladka;
 
     public Polisa(String typ, double skladka) {
         this.typ = typ;
-        this.skladka=skladka;
+        this.skladka = skladka;
     }
 
     public String getTyp() {
         return typ;
     }
+
     public double getSkladka() {
         return skladka;
     }
