4cbbbc9766ddc7ab8d8f0e5667d733aea2680457
Bartosz Krawczyk <144348029+Krawczyk0@users.noreply.github.com>
2023-12-02 22:29:48 +0100

    Update SystemUbezpieczeniowy.java

--- src/pl/gornik/SystemUbezpieczeniowy.java
+++ src/pl/gornik/SystemUbezpieczeniowy.java
@@ -7,8 +7,7 @@ import java.util.Scanner;
 public class SystemUbezpieczeniowy {
     public static void main(String[] args) {
         Klient.powitanie();
-        List<Klient> listaKlientow = new ArrayList<>();
-        initializeKlient(listaKlientow);
+        List<Klient> listaKlientow = ListaKlientow();
 
         int wybor;
         Scanner scanner = new Scanner(System.in);
@@ -121,7 +120,7 @@ public class SystemUbezpieczeniowy {
                     break;
                 case 8:
                     System.out.print("Podaj nazwę pliku z danymi klienta: ");
-                    scanner.nextLine(); 
+                    scanner.nextLine();
                     String nazwaPliku = scanner.nextLine();
                     Klient klientZPliku = Klient.wczytajZPliku(nazwaPliku);
                     if (klientZPliku != null) {
@@ -138,13 +137,78 @@ public class SystemUbezpieczeniowy {
             }
         } while (wybor != 9);
     }
+    private static List<Klient> ListaKlientow() {
+        List<Klient> lista = new ArrayList<>();
 
-    public static void initializeKlient(List<Klient> listaKlientow) {
-        listaKlientow.add(new Klient("Jan", "Kowalski"));
-        listaKlientow.add(new Klient("Zbyszek","Nowak"));
-        listaKlientow.add(new Klient("Paweł","Stolarski"));
-        listaKlientow.add(new KlientVIP("Izydor","Komorowski",5));
-        listaKlientow.add(new KlientVIP("Henryk","Belek",3));
-        listaKlientow.add(new KlientVIP("Jacek","Baran",2.5));
+        Klient klient1 = new Klient("Jan", "Kowalski");
+        klient1.dodajPolise("Auto", 1200.0);
+        klient1.dodajPolise("Mieszkanie", 1800.0);
+
+        Klient klient2 = new Klient("Anna", "Nowak");
+        klient2.dodajPolise("Zdrowie", 800.0);
+        klient2.dodajPolise("Życie", 1500.0);
+
+        Klient klient3 = new KlientVIP("Marek", "Kwiatkowski", 0.1);
+        klient3.dodajPolise("Dom", 2500.0);
+        klient3.dodajPolise("Odpowiedzialność cywilna", 1200.0);
+
+        Klient klient4 = new Klient("Katarzyna", "Wójcik");
+        klient4.dodajPolise("Auto", 1300.0);
+        klient4.dodajPolise("Mieszkanie", 2000.0);
+
+        Klient klient5 = new Klient("Piotr", "Lis");
+        klient5.dodajPolise("Zdrowie", 900.0);
+        klient5.dodajPolise("Życie", 1700.0);
+
+        Klient klient6 = new KlientVIP("Agnieszka", "Nowicka", 0.15);
+        klient6.dodajPolise("Dom", 2800.0);
+        klient6.dodajPolise("Odpowiedzialność cywilna", 1300.0);
+
+        Klient klient7 = new Klient("Michał", "Kowalczyk");
+        klient7.dodajPolise("Auto", 1400.0);
+        klient7.dodajPolise("Mieszkanie", 2200.0);
+
+        Klient klient8 = new Klient("Monika", "Szymańska");
+        klient8.dodajPolise("Zdrowie", 950.0);
+        klient8.dodajPolise("Życie", 1800.0);
+
+        Klient klient9 = new KlientVIP("Grzegorz", "Kowalski", 0.2);
+        klient9.dodajPolise("Dom", 3000.0);
+        klient9.dodajPolise("Odpowiedzialność cywilna", 1400.0);
+
+        lista.add(klient1);
+        lista.add(klient2);
+        lista.add(klient3);
+        lista.add(klient4);
+        lista.add(klient5);
+        lista.add(klient6);
+        lista.add(klient7);
+        lista.add(klient8);
+        lista.add(klient9);
+
+        return lista;
+    }
+    private static Klient dodajNowegoKlienta() {
+        Scanner scanner = new Scanner(System.in);
+        String imie;
+        String nazwisko;
+
+        do {
+            System.out.print("Podaj imię klienta (zaczynając od wielkiej litery): ");
+            imie = scanner.nextLine().trim();
+            if (imie.isEmpty() || !Character.isUpperCase(imie.charAt(0))) {
+                System.out.println("Imię nie może być puste i musi zaczynać się od wielkiej litery. Podaj ponownie.");
+            }
+        } while (imie.isEmpty() || !Character.isUpperCase(imie.charAt(0)));
+
+        do {
+            System.out.print("Podaj nazwisko klienta (zaczynając od wielkiej litery): ");
+            nazwisko = scanner.nextLine().trim();
+            if (nazwisko.isEmpty() || !Character.isUpperCase(nazwisko.charAt(0))) {
+                System.out.println("Nazwisko nie może być puste i musi zaczynać się od wielkiej litery. Podaj ponownie.");
+            }
+        } while (nazwisko.isEmpty() || !Character.isUpperCase(nazwisko.charAt(0)));
+
+        return new Klient(imie, nazwisko);
     }
 }
